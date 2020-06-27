ad3cf8460850cc87d955cb6e127ddafc7712d644
Henry Brophy <henrybrophy@Henrys-MacBook-Pro.local>
2020-06-26 20:31:09 -0400

    Added 3 tables.

--- data/migrations/20200626195513_projects.js
+++ data/migrations/20200626195513_projects.js
@@ -4,8 +4,22 @@ exports.up = async function (knex) {
     table.text("name").notNull;
     table.text("description");
   });
+
+  await knex.schema.createTable("resources", (table) => {
+    table.increments();
+    table.text("name").unique();
+    table.text("description");
+  });
+
+  await knex.schema.createTable("task", (table) => {
+    table.increments();
+    table.text("description");
+    table.text("notes");
+  });
 };
 
 exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("task");
+  await knex.schema.dropTableIfExists("resources");
   await knex.schema.dropTableIfExists("project");
 };
