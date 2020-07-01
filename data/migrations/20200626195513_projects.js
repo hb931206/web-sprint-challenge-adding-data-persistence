cca665115b12eb22480f6a0f35bc2f5585be85a9
Henry Brophy <henrybrophy@Henrys-MacBook-Pro.local>
2020-06-30 21:10:33 -0400

    Modified tables and added the foreign key table.

--- data/migrations/20200626195513_projects.js
+++ data/migrations/20200626195513_projects.js
@@ -1,8 +1,9 @@
 exports.up = async function (knex) {
   await knex.schema.createTable("project", (table) => {
     table.increments();
-    table.text("name").notNull;
+    table.text("name").notNull();
     table.text("description");
+    table.boolean("completed").defaultTo(false).notNull();
   });
 
   await knex.schema.createTable("resources", (table) => {
@@ -15,10 +16,17 @@ exports.up = async function (knex) {
     table.increments();
     table.text("description");
     table.text("notes");
+    table.boolean("completed").defaultTo(false).notNull();
+  });
+
+  await knex.schema.createTable("project_resources", (table) => {
+    table.integer("project_id").references("id").inTable("project");
+    table.integer("resource_id").references("id").inTable("resources");
   });
 };
 
 exports.down = async function (knex) {
+  await knex.schema.dropTableIfExists("project_resources");
   await knex.schema.dropTableIfExists("task");
   await knex.schema.dropTableIfExists("resources");
   await knex.schema.dropTableIfExists("project");
