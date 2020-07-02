394db2fdd44997f850746c555d0d74b656b99bdf
Henry Brophy <henrybrophy@Henrys-MBP.lan>
2020-07-02 15:59:24 -0400

    Added the add_resource endpoint.

--- routers/resource.js
+++ routers/resource.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const db = require("../data/config");
 const Resource = require("../model/resource");
 
 const router = express.Router();
@@ -7,6 +8,16 @@ router.get("/resources", async (req, res, next) => {
   try {
     const resources = await Resource.find();
     res.json(resources);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post("/resources", async (req, res, next) => {
+  try {
+    const [id] = await db("resource").insert(req.body);
+    const resPost = await db("resource").where({ id }).first();
+    res.status(201).json(resPost);
   } catch (err) {
     next(err);
   }
