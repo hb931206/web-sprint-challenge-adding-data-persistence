badcfcec574cefd44d22b41caae911558b74ecc8
Henry Brophy <henrybrophy@Henrys-MBP.lan>
2020-07-02 15:08:50 -0400

    Testing to see if I can get endpoints that don't require project_resource.

--- data/seeds/resource.js
+++ data/seeds/resource.js
@@ -1,14 +1,8 @@
 exports.seed = async function (knex) {
   await knex("resource").insert([
     {
-      id: 1,
       name: "Projector",
       description: "For our client meeting",
-    },
-    {
-      id: 2,
-      name: "Computer",
-      description: "Need this for the presentation",
     },
   ]);
 };
--- data/seeds/task.js
+++ data/seeds/task.js
@@ -1,15 +1,8 @@
 exports.seed = async function (knex) {
   await knex("task").insert([
     {
-      id: 1,
       description: "Prep for the meeting",
       notes: "The meeting is Friday",
-      completed: false,
-    },
-    {
-      id: 2,
-      description: "Debrief after the call",
-      notes: "The call went well",
       completed: false,
     },
   ]);
